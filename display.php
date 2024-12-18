e54047bbb0ae1f5eeb06b7cdd6187d83a31fcdab
BharatSharma03 <apradhiyt21@gmail>
2024-12-18 18:38:38 +0530

    create contact page and also link with the database

--- display.php
+++ display.php
@@ -31,7 +31,7 @@ if ($conn->connect_error) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-  <?php require 'partials/_nav.php' ?>
+  <?php require 'partials/bars/_nav.php' ?>
 
   <div class="container">
     <h1 class="text-center">User Information</h1>
