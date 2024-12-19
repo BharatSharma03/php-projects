55d2d357610f9f7b56f930460215493eb91c4b40
BharatSharma03 <apradhiyt21@gmail>
2024-12-19 17:39:22 +0530

    start to manage A  crousal

--- partials/bars/sidebar.php
+++ partials/bars/sidebar.php
@@ -12,8 +12,7 @@
     <div class="w3-sidebar w3-bar-block w3-card w3-animate-right" style="display:none;right:0;" id="rightMenu">
       <button onclick="closeRightMenu()" class="w3-bar-item w3-button w3-large">Close &times;</button>
       <a href="user_manage.php" target="_parent" class="w3-bar-item w3-button">user manages</a>
-      <a href="#" class="w3-bar-item w3-button">Link 2</a>
-      <a href="#" class="w3-bar-item w3-button">Link 3</a>
+      <a href="slider.php" class="w3-bar-item w3-button">manageslider</a>
       <a href="login.php" class="w3-bar-item w3-button">logout</a>
     </div>
 
